7a200512bd278d081a653ee073db8c40391eb260
JebDev <jebdesign91@gmail.com>
2025-07-25 10:36:20 +0100

    jsx questions correct/wrong

--- src/Components/QuizQuestions/Quiz.js
+++ src/Components/QuizQuestions/Quiz.js
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useState } from 'react'
+import './Quiz.css'
+import { data } from "../../Assets/data"
 
-class Quiz extends React.Component{
+const Quiz = () => {
+    let [index,setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index]);
+    let [lock,setLock]  = useState(false);
 
-    render() {
-        return(
-            <div className="quiz">
-            <h2 className='question'>question</h2>
-            <div className='answer-buttons'>
-              <button className='btn'>Answer 1</button>
-              <button className='btn'>Answer 1</button>
-              <button className='btn'>Answer 1</button>
-              <button className='btn'>Answer 1</button>
-            </div>
-            <div>
-             <button className='nxt-btn'>Next</button>
-            </div>
-          </div>
-        )
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let option_array = [Option1,Option2,Option3,Option4];
+
+    const checkAns = (e,ans) => {
+        if (lock === false) {
+            if (question.ans===ans) {
+                e.target.classList.add("correct");
+                setLock(true)
+              }
+              else{
+                e.target.classList.add("wrong");
+                setLock(true);
+                option_array[question.ans-1].current.classList.add("correct");
+            } 
+        }
+     
     }
-}
+
+    return(
+        <div className="container">
+        <h2>{index+1}. {question.question}</h2>
+        <hr />
+        <ul>
+        <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+        <li  ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+        <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+        <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+         </ul>
+         <button>Next</button>
+        <div className='index' >1 of 5 questions</div>
+      </div>
+    )
+};
+
+
+    
+   
+       
+    
+
 
 export default Quiz;
